40093cd9fb2cf7139fb2229fccce4506c0dc3663
Natanael Rabello <Natanael Rabello>
2017-03-04 19:47:24 -0300

    Add misc files

---
+++ src/todo.h
@@ -0,0 +1,15 @@
+#define LINK_LED         LED_GREEN
+#define CHG_LED          LED_GREEN
+#define LOWBAT_LED       LED_RED
+#define LINK_DOWN_LED    LED_GREEN
+#define SYS_LED          LED_RED
+#define ERR_LED1         LED_RED
+#define ERR_LED2         LED_GREEN
+
+//declare var isInit for every class
+
+/**
+ * \def ACTIVATE_STARTUP_SOUND
+ * Playes a startup melody using the motors and PWM modulation
+ */
+#define ACTIVATE_STARTUP_SOUND
